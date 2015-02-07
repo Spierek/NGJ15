e8c43940c498c93f818d634cef6daa5e8d3101c1
Tomek Paszek <tomek@unity3d.com>
2015-02-07 19:05:28 +0100

    added start scene template

---
+++ Unity/Assets/Scripts/MenuController.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuController : MonoBehaviour {
+
+
+	public GameObject Screen1;
+	public GameObject Screen2;
+	public GameObject Screen3;
+	public GameObject Screen4;
+	
+	// Use this for initialization
+	void Start () {
+		Screen1.SetActive(true);
+		Screen2.SetActive(false);
+		Screen3.SetActive(false);
+		Screen4.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Space))
+		{
+			//if(anyButtonPressed)
+
+			if(Screen1.activeInHierarchy)
+			{
+				Screen1.SetActive(false);
+				Screen2.SetActive(true);
+			}else
+
+			if(Screen2.activeInHierarchy)
+			{
+				Screen2.SetActive(false);
+				Screen3.SetActive(true);
+			}else
+
+			if(Screen3.activeInHierarchy)
+			{
+				Screen3.SetActive(false);
+				Screen4.SetActive(true);
+			}else
+
+			if(Screen4.activeInHierarchy)
+			{
+				Application.LoadLevel(1);
+			}
+		}
+
+	}
+}
