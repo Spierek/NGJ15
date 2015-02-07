6acebb68f6022c0a3947d01b22a501a907df576f
Ret <retro44@gmail.com>
2015-02-07 14:05:57 +0100

    [+] Background.cs file

--- Unity/Assets/background.cs
+++ Unity/Assets/background.cs
@@ -1,8 +1,22 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class background : MonoBehaviour {
+public class Background : MonoBehaviour {
 
+    public float speed;
+
+    private Transform[] childs;
+
+    void Awake()
+    {
+        childs = new Transform[transform.childCount];
+        for (int i = 0; i < transform.childCount; i++ )
+        {
+            childs[i] = transform.GetChild(i);
+            childs[i].localPosition = new Vector3(0f, 10f * i, 0f);
+        }
+    }
+  
 	// Use this for initialization
 	void Start () {
 	
@@ -10,6 +24,12 @@ public class background : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+
+        for (int i = 0; i < childs.Length; i++)
+        {
+            childs[i].localPosition -= transform.up * speed * Time.deltaTime;
+            if (childs[i].localPosition.y <= -10f) childs[i].localPosition += new Vector3(0f, 10f * childs.Length, 0f);
+        }
+          
 	}
 }
