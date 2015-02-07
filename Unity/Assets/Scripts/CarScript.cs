ce6436194aac8b58964cdb470e98008a8cd54408
Luke Spierewka <spierek@gmail.com>
2015-02-07 16:55:45 +0100

    [+] New car graphics [+] Rotating car wheel [*] Improved & smoother car movement

--- Unity/Assets/Scripts/CarScript.cs
+++ Unity/Assets/Scripts/CarScript.cs
@@ -2,13 +2,17 @@
 
 public class CarScript : MonoBehaviour {
     #region Variables
-    private new Rigidbody   rigidbody;
-    private Transform       cameraBox;
+    public Transform        wheel;
     
     public float distanceLimit = 5f;
+    public float wheelRotationSpeed = 7f;
+    public float wheelRotationLimit = 60f;
 
     private float shakePower;
     private float shakeDuration;
+
+    private new Rigidbody   rigidbody;
+    private Transform       cameraBox;
     #endregion
 
     #region Monobehaviour Methods
@@ -18,9 +22,12 @@ public class CarScript : MonoBehaviour {
     }
 
     void Update () {
-        Movement();
-
+        RotateWheel();
         CameraShaking();
+    }
+
+    void FixedUpdate() {
+        Movement();
     }
     #endregion
 
@@ -28,13 +35,18 @@ public class CarScript : MonoBehaviour {
     private void Movement() {
         // get input and add force
         Vector2 movementVector = new Vector2(Input.GetAxis("Horizontal"), 0);
-        rigidbody.AddForce(movementVector * 2, ForceMode.Impulse);
+        rigidbody.AddForce(movementVector * 1, ForceMode.Impulse);
 
         // bounce off of walls (distance clamping)
         if ((transform.position.x < -distanceLimit && rigidbody.velocity.x < 0) || (transform.position.x > distanceLimit && rigidbody.velocity.x > 0)) {
             rigidbody.velocity = -2 * rigidbody.velocity;
             ShakeCamera(0.1f, 2f);
         }
+    }
+
+    private void RotateWheel() {
+        wheel.Rotate(0, 0, -Input.GetAxis("Horizontal") * wheelRotationSpeed);
+        wheel.localRotation = Quaternion.Euler(0, 0, ClampAngle(wheel.localEulerAngles.z, -wheelRotationLimit, wheelRotationLimit));
     }
 
     public void ShakeCamera(float duration, float power) {
@@ -52,5 +64,17 @@ public class CarScript : MonoBehaviour {
             cameraBox.localPosition = Vector3.zero;
         }
     }
+
+    // from http://answers.unity3d.com/questions/141775/limit-local-rotation.html
+    private float ClampAngle(float angle, float min, float max) {
+     if (angle<90 || angle>270){       // if angle in the critic region...
+         if (angle>180) angle -= 360;  // convert all angles to -180..+180
+         if (max>180) max -= 360;
+         if (min>180) min -= 360;
+     }    
+     angle = Mathf.Clamp(angle, min, max);
+     if (angle<0) angle += 360;  // if angle negative, convert to 0..360
+     return angle;
+ }
     #endregion
 }
