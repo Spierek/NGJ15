dad1c31f10f6ca330c7817b02d9cb0a53274191a
marny568 <marny568@student.liu.se>
2015-02-07 15:26:29 +0100

    Added so that key presses changes then womans face color

---
+++ Unity/Assets/Scripts/taxidriverWomanInteraction.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class taxidriverWomanInteraction : MonoBehaviour {
+
+	//public var woman = GameObject.Find ("Woman").GetComponent<Cube>();
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var woman = GameObject.Find ("Woman");
+
+		if (Input.GetKey(KeyCode.A))
+			woman.renderer.material.color = Color.red;
+
+		if (Input.GetKey(KeyCode.S))
+		    woman.renderer.material.color = Color.blue;
+
+		if (Input.GetKey(KeyCode.D))
+		    woman.renderer.material.color = Color.green;
+		
+	}
+}
