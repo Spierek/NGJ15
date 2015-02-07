095ac7669513a4fa11d3ac8917f956c566468a59
Ret <retro44@gmail.com>
2015-02-07 14:54:52 +0100

    [+] Buildings prototype

---
+++ Unity/Assets/Scripts/LookAtCamera.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LookAtCamera : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.LookAt(transform.position + Camera.main.transform.rotation * Vector3.back, Camera.main.transform.rotation * Vector3.up);
+	}
+}
