bd3345dc7e55fac6f49f33817fdea6aee3cce875
Tomek Paszek <tomek@unity3d.com>
2015-02-08 14:34:15 +0100

    baby cry updated

---
+++ Unity/Assets/BabySoundController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BabySoundController : MonoBehaviour {
+
+
+	static bool playing;
+
+	void Start () {
+		
+		var audio = gameObject.GetComponent<AudioSource>();
+		if(!playing)
+		{
+			playing = true;
+			audio.Play();
+		}
+	}
+}
